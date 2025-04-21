8ade16ae2dd969309e21c33da1adfe391ed1715e
hamzaraza123 <148256295+hamzaraza123@users.noreply.github.com>
2025-04-22 02:36:57 +0500

    Add files via upload

---
+++ TASK 3/Program_Counter_task3.v
@@ -0,0 +1,20 @@
+module Program_Counter_task3(
+  input [63:0] PC_in,
+  input clk,
+  input reset,
+  input stall, 
+  output reg [63:0] PC_out);
+  
+  always @(posedge clk or posedge reset)
+    begin
+      if (reset==1'b1)
+        begin        
+          PC_out = 64'd0;
+        end
+      else if (stall == 1'b0) 
+        begin 
+          PC_out = PC_in;
+        end
+    end
+endmodule
+
---
+++ TASK 3/RISC_V_Processor_task3.v
@@ -0,0 +1,311 @@
+module RISC_V_Processor_task3(
+  input clk,
+  input reset,
+  input wire[63:0] element1,
+  input wire[63:0] element2,
+  input wire[63:0] element3,
+  input wire[63:0] element4,
+  input wire[63:0] element5,
+  input wire[63:0] element6,
+  input wire[63:0] element7,
+  input wire[63:0] element8,
+  input stall, flush);
+  
+  wire branch;
+  wire memread;
+  wire memtoreg;
+  wire memwrite;
+  wire ALUsrc;
+  wire regwrite;
+  wire [1:0] ALUop;
+  
+  wire regwrite_memwb_out;
+  wire [63:0] readdata1, readdata2;
+  wire [63:0] r8, r19, r20, r21, r22;
+  wire [63:0] write_data;
+  
+  wire [63:0] pc_in;
+  wire [63:0] pc_out;
+  
+  wire [63:0] adderout1;
+  wire [63:0] adderout2;
+  
+  wire [31:0] instruction;
+  wire[31:0] inst_ifid_out;
+  
+  wire [6:0] opcode;
+  wire [4:0] rd, rs1, rs2;
+  wire [2:0] funct3;
+  wire [6:0] funct7;
+  
+  
+  wire [63:0] imm_data;
+   
+  wire [63:0] random;
+ 
+  wire [63:0] a1;
+  wire [4:0] RS1;
+  wire [4:0] RS2;
+  wire [4:0] RD;
+  wire [63:0] d, M1, M2;
+  wire Branch;
+  wire Memread;
+  wire Memtoreg;
+  wire Memwrite;
+  wire Regwrite;
+  wire Alusrc;
+  wire [1:0] aluop;
+  wire [3:0] funct4_out;
+  
+  wire [63:0] threeby1_out1;
+  wire[63:0] threeby1_out2;
+  wire[63:0]  alu_64_b;
+  
+  wire [63:0] write_Data;
+  wire [63:0] exmem_out_adder;
+  wire exmem_out_zero;
+  wire [63:0] exmem_out_result;
+  wire [4:0] exmemrd;
+  wire BRANCH,MEMREAD,MEMTOREG,MEMEWRITE,REGWRITE;   
+  
+  wire [63:0] AluResult;
+  wire zero;
+  
+  wire [3:0] operation;
+  
+  wire [63:0] readdata;
+ 
+  wire[63:0] muxin1,muxin2;
+  wire [4:0] memwbrd;
+  wire memwb_memtoreg;
+  wire memwb_regwrite;
+  
+  wire [1:0] forwardA;
+  wire [1:0] forwardB;
+  
+  wire addermuxselect;
+  wire branch_final;
+  
+  
+  pipeline_flush p_flush
+  (
+    .branch(branch_final & BRANCH),
+    .flush(flush)
+  );
+  
+  
+  hazard_detection_unit hu
+  (
+    .Memread(Memread),
+    .inst(inst_ifid_out),
+    .Rd(RD),
+    .stall(stall)
+  );
+
+  
+  Program_Counter_task3 pc 
+  (
+    .PC_in(pc_in),
+    .stall(stall),
+    .clk(clk),
+    .reset(reset),
+    .PC_out(pc_out)
+  );
+  
+  Instruction_Memory im
+  (
+    .Inst_Address(pc_out),
+    .Instruction(instruction)
+  );
+  
+  
+  Adder adder1
+  (
+    .a(pc_out),
+    .b(64'd4),
+    .out(adderout1)
+  );
+  
+   ifidreg_task3 i1 
+  (
+    .clk(clk),
+    .reset(reset),
+    .IFIDWrite(stall),
+    .instruction(instruction),
+    .A(pc_out),
+    .inst(inst_ifid_out),
+    .a_out(random),
+    .flush(flush)
+  );
+  
+  InsParser ip
+  (
+    .instruction(inst_ifid_out),
+    .opcode(opcode),
+    .rd(rd),
+    .funct3(funct3),
+    .rs1(rs1),
+    .rs2(rs2),
+    .funct7(funct7)
+  );
+  
+  Control_Unit_task3 cu
+  (
+    .opcode(opcode),
+    .branch(branch),
+    .memread(memread),
+    .memtoreg(memtoreg),
+    .memwrite(memwrite),
+    .aluSrc(ALUsrc),
+    .regwrite(regwrite),
+    .Aluop(ALUop),
+    .stall(stall)
+  );
+  
+  data_extractor immextr
+  (
+    .instruction(inst_ifid_out),
+    .imm_data(imm_data)
+  );
+  
+  
+  registerFile_task3 regfile 
+  (
+    .clk(clk),
+    .reset(reset),
+    .rs1(rs1),
+    .rs2(rs2),
+    .rd(memwbrd),
+    .writedata(write_data),
+    .reg_write(memwb_regwrite),
+    .readdata1(readdata1),
+    .readdata2(readdata2),
+    .r8(r8),
+    .r19(r19),
+    .r20(r20),
+    .r21(r21),
+    .r22(r22)
+  );
+  
+  idexreg_task3 i2
+  (
+    .clk(clk),
+    .flush(flush),
+    .reset(reset),
+    .funct4_in({inst_ifid_out[30],inst_ifid_out[14:12]}),
+    .A_in(random),
+    .readdata1_in(readdata1),
+    .readdata2_in(readdata2),
+    .imm_data_in(imm_data),
+    .rs1_in(rs1),.rs2_in(rs2),.rd_in(rd),
+    .branch_in(branch),.memread_in(memread),.memtoreg_in(memtoreg),
+    .memwrite_in(memwrite),.aluSrc_in(ALUsrc),.regwrite_in(regwrite),.Aluop_in(ALUop),
+    .a(a1),.rs1(RS1),.rs2(RS2),.rd(RD),.imm_data(d),.readdata1(M1),.readdata2(M2),
+    .funct4_out(funct4_out),.Branch(Branch),.Memread(Memread),.Memtoreg(Memtoreg),
+    .Memwrite(Memwrite),.Regwrite(Regwrite),.Alusrc(Alusrc),.aluop(aluop)
+  );
+  Adder adder2
+  (
+    .a(a1),
+    .b(d << 1),
+    .out(adderout2)
+  );
+  
+  
+  mux_3to1 m1
+  (
+    .a(M1),.b(write_data),.c(exmem_out_result),.sel(forwardA),.out(threeby1_out1)
+  );
+  
+  mux_3to1 m2
+  (
+    .a(M2),.b(write_data),.c(exmem_out_result),.sel(forwardB),.out(threeby1_out2)
+  );
+  
+  Mux mux1
+  (
+    .a(threeby1_out2),.b(d),.sel(Alusrc),.data_out(alu_64_b)
+  );
+  
+  ALU_64_bit alu
+  (
+    .a(threeby1_out1),
+    .b(alu_64_b),
+    .ALUOp(operation),
+    .Result(AluResult),
+    .ZERO(zero)
+  );
+  
+   ALU_Control ac
+  (
+    .ALUOp(aluop),
+    .Funct(funct4_out),
+    .Operation(operation)
+  );
+  
+ 
+  
+  exmemreg_task3 i3
+  (
+    .clk(clk),.reset(reset),.Adder_out(adderout2),.Result_in_alu(AluResult),.Zero_in(zero),.flush(flush),
+    .writedata_in(threeby1_out2),.Rd_in(RD), .addermuxselect_in(addermuxselect),
+    .branch_in(Branch),.memread_in(Memread),.memtoreg_in(Memtoreg),.memwrite_in(Memwrite),.regwrite_in(Regwrite),
+    .Adderout( exmem_out_adder),.zero(exmem_out_zero),.result_out_alu(exmem_out_result),.writedata_out(write_Data),
+    .rd(exmemrd),.Branch(BRANCH),.Memread(MEMREAD),.Memtoreg(MEMTOREG),.Memwrite(MEMEWRITE),.Regwrite(REGWRITE), .addermuxselect(branch_final)
+  );
+    
+  Data_Memory datamem
+  (
+    .Write_Data(write_Data),
+    .Mem_Addr(exmem_out_result),
+    .MemWrite(MEMEWRITE),
+    .clk(clk),
+    .MemRead(MEMREAD),
+    .Read_Data(readdata),
+    .element1(element1),
+    .element2(element2),
+    .element3(element3),
+    .element4(element4),
+    .element5(element5),
+    .element6(element6),
+    .element7(element7),
+    .element8(element8)
+  );
+  
+  
+  Mux mux2
+  (
+    .a(adderout1),.b(exmem_out_adder),.sel(BRANCH & branch_final),.data_out(pc_in) // ??
+  );
+
+ 
+  memwbreg_task3 i4
+  
+  (
+    .clk(clk),.reset(reset),.read_data_in(readdata),
+    .result_alu_in(exmem_out_result),.Rd_in(exmemrd),.memtoreg_in(MEMTOREG),.regwrite_in(REGWRITE),
+    .readdata(muxin1),.result_alu_out(muxin2),.rd(memwbrd),.Memtoreg(memwb_memtoreg),.Regwrite(memwb_regwrite)
+  );
+  
+   Mux mux3
+  (
+    .a(muxin2),.b(muxin1),.sel(memwb_memtoreg),.data_out(write_data)
+  );
+  
+  
+   forwardingunit f1
+  (
+    .RS_1(RS1),.RS_2(RS2),.rdMem(exmemrd),
+    .rdWb(memwbrd),.regWrite_Wb(memwb_regwrite),
+    .regWrite_Mem(REGWRITE),
+    .Forward_A(forwardA),.Forward_B(forwardB)
+  );
+  
+  
+  branching_unit branc
+  (
+    .funct3(funct4_out[2:0]),.readData1(M1),.b(alu_64_b),.addermuxselect(addermuxselect)
+  );
+
+        
+endmodule 
