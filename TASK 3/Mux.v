b061c5bfefae555af919194f6a9e15793f239fb4
hamzaraza123 <148256295+hamzaraza123@users.noreply.github.com>
2025-04-22 02:38:13 +0500

    Add files via upload

---
+++ TASK 3/Mux.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module Mux
+(
+input [63:0] a,
+input [63:0] b,
+input sel,
+output reg [63:0] data_out
+);
+
+always @ (a, b, sel)
+begin
+if (!sel)
+data_out = a;
+else
+data_out = b;
+end
+endmodule
+
